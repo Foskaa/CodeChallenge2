241d3a7a948d410b937e6a401f0b0e01cea169f5
Foskaa <bobbysudiharto3@gmail.com>
2024-10-08 16:10:54 +0700

    twentytwo

--- src/features/home/components/bannersection/index.jsx
+++ src/features/home/components/bannersection/index.jsx
@@ -2,23 +2,25 @@ import Banner from '@/images/banner.webp'
 import Image from 'next/image'
 
 export default function BannerSection() {
-    return(
-    <section id="banner">
-        <Image
-            src={Banner}
-            alt="Banner"
-            loading="lazy"
-            className="flex"
-            decoding="async"
-            srcSet={`
+    return (
+        <section id="banner">
+            <Image
+                src={Banner}
+                alt="Banner"
+                loading="lazy"
+                className="flex"
+                decoding="async"
+                width={1536}
+                height={786}
+                srcSet={`
     /_next/image?url=${Banner}&w=640&q=75 640w,
     /_next/image?url=${Banner}&w=1280&q=75 1280w,
     /_next/image?url=${Banner}&w=1920&q=75 1920w,
     /_next/image?url=${Banner}&w=3840&q=75 3840w
   `}
-            sizes="(max-width: 640px) 640px, (max-width: 1280px) 1280px, (max-width: 1920px) 1920px, 100vw"
+                sizes="(max-width: 640px) 640px, (max-width: 1280px) 1280px, (max-width: 1920px) 1920px, 100vw"
 
-        />
+            />
         </section>
     )
 }
